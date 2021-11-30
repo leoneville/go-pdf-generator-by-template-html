6318bab053134f0696adf6e4caeae9af708f99ec
leoneville <leoneville_@hotmail.com>
2021-11-30 00:21:15 -0300

    First Commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module generatorPDF
+
+go 1.17
+
+require github.com/google/uuid v1.3.0
+
+require github.com/SebastiaanKlippert/go-wkhtmltopdf v1.7.2
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"generatorPDF/htmlParser"
+	"generatorPDF/pdfGenerator"
+	"os"
+)
+
+type Data struct {
+	Name  string
+	Texto string
+}
+
+func main() {
+
+	h := htmlParser.New("tmp")
+	wk := pdfGenerator.New("tmp")
+
+	dataHTML := Data{
+		Name:  "Neville",
+		Texto: "Seu cartao ja se encontra desbloqueado.",
+	}
+
+	htmlGenerated, err := h.Create("templates/example.html", dataHTML)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer os.Remove(htmlGenerated)
+
+	fmt.Println("HTML gerado", htmlGenerated)
+
+	filePDFName, err := wk.Create(htmlGenerated)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("PDF gerado", filePDFName)
+
+}
